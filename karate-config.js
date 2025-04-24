c0ea9cd95af7260c1001243924f9756bfa7fff73
Lyubo Lyubenov <lyubomir.lyubenov@remixshop.com>
2025-04-24 08:59:16 +0300

    Create basic karate sctructure for standalone

---
+++ karate-config.js
@@ -0,0 +1,32 @@
+function fn() {
+  var env = karate.env;  //  <set this only when set for CI execution - karate parameter e=locale
+  karate.log('karate.env system property was:', env);
+  
+  if (!env) {
+    env = 'dev'; // a custom 'intelligent' default
+  }
+    //Default defaults (dev environment + bg market)
+    var config = {baseURL: 'https://qa-assessment.pages.dev/'}; 
+    function getResourcePath(resource) {
+      var File = Java.type('java.io.File');
+      var Paths = Java.type('java.nio.file.Paths');
+      var path = Paths.get('util', resource).toAbsolutePath().toString();
+      return path;
+    }
+    var extensionPath = getResourcePath('chrome-extension');
+  
+  
+    //Website
+    if (env == 'dev') {
+      config.baseURL = 'https://qa-assessment.pages.dev/'
+      
+     }
+     
+    // don't waste time waiting for a connection or if servers don't respond within 15 seconds
+    karate.configure('retry', { count: 6, interval: 3000 });
+    karate.configure('connectTimeout', 25000);
+    karate.configure('readTimeout', 20000);
+
+    //Locally Chrome:
+    karate.configure('driver', { type: 'chrome', addOptions: ["--remote-allow-origins=*", '--disable-search-engine-choice-screen', '--disable-infobars','--no-first-run','--disable-notifications', '--disable-features=PasswordLeakDetection'], showDriverLog: false });
+    }
